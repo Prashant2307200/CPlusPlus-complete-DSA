2c394ee64b2f8375c1512aca1c0e5d42c1d0fff7
Prashant Dobariya <118981177+Prashant2307200@users.noreply.github.com>
2023-01-05 04:39:25 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@1Output-Inputcpp/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  std::cout << "Hello World!\n";
+
+  int amount1;
+  cout<<"Enter amount1 ";
+  cin>>amount1;
+
+  int amount2;
+  cout<<"Enter amount2 ";
+  cin>>amount2;
+
+  int sum=amount1+amount2;
+  cout<<"Net amount is "<<sum<<endl;
+}
